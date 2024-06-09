d83a3a7f2368c0a79f9ba19db5835b74fcbafcd2
Marat Lanza <lanzamarat@gmail.com>
2024-06-09 11:49:17 -0600

    export components

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+export * from "./components/index";
+
+// Charts components
+export * from './components/charts/barChart'
+export * from './components/charts/lineChart'
+
+// ui components
+export * from './components/ui/accordion'
+export * from './components/ui/alert-dialog'
+export * from './components/ui/alert'
+export * from './components/ui/button'
+export * from './components/ui/card'
+export * from './components/ui/checkbox'
+export * from './components/ui/dialog'
+export * from './components/ui/hover-card'
+export * from './components/ui/input'
+export * from './components/ui/popover'
+export * from './components/ui/radio-group'
+export * from './components/ui/skeleton'
+export * from './components/ui/switch'
+export * from './components/ui/table'
+export * from './components/ui/tabs'
+export * from './components/ui/toast'
+export * from './components/ui/toaster'
+export * from './components/ui/tooltip'
+
+
